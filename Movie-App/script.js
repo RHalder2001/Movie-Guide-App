d40bfac0ff667c89a504531f95e63a8308475351
RUPAK HALDER <rupakhalder999@gmail.com>
2025-07-24 17:06:59 +0000

    All change made

--- Movie-App/script.js
+++ Movie-App/script.js
@@ -16,35 +16,61 @@ const getMovieInfo = async (movie) => {
     showMovieData(data);
   } catch (error) {
     console.log("E: ", error);
+    movieContainer.innerHTML="<h2>No Movie Found</h2>"
+     movieContainer.classList.remove('movie-container')
+    movieContainer.classList.add('noBackground')
   }
 };
 
 // Function to show movie data on screen
 const showMovieData = (data) => {
+  movieContainer.innerHTML = ""
+   movieContainer.classList.remove('noBackground')
+
+   //Use Destructuring data(object) come from API
   const { Title, imdbRating, Genre, Released, Runtime, Actors, Plot, Poster } = data;
 
   const movieElement = document.createElement('div');
-  movieElement.innerHTML = 
-       `<h2>${Title}</h2>
+  movieElement.classList.add('movie-info')
+  movieElement.innerHTML =
+    `<h2>${Title}</h2>
         <p><strong>Rating: &#11088</strong>${imdbRating}</p>`
 
 
 
-    //creating Movie Genre with div
-    const movieGenreElement =document.createElement('div')
-     movieGenreElement.classList.add('movie-genre')
+  //creating Movie Genre with div
+  const movieGenreElement = document.createElement('div')
+  movieGenreElement.classList.add('movie-genre')
 
-     Genre.split(",").forEach(element=>{
-      const p=document.createElement('p')
-      p.innerText=element;
-      movieGenreElement.appendChild(p)
+  Genre.split(",").forEach((element) => {
+    const p = document.createElement('p')
+    p.innerText = element;
+    movieGenreElement.appendChild(p)
 
-     })
+  })
 
-   
-
-  movieContainer.appendChild(movieElement);
   movieElement.appendChild(movieGenreElement);
+
+
+
+
+  movieElement.innerHTML +=
+    `<p><strong>Release Data: </strong>${Released}</p>
+      <p><strong>Duration: </strong>${Runtime}</p>
+      <p><strong>Cast: </strong>${Actors}</p>
+      <p><strong>Plot: </strong>${Plot}</p>`
+
+
+
+  //Creating div for movie poster
+  const moviePosterElement = document.createElement('div')
+  moviePosterElement.classList.add('movie-poster')
+
+  moviePosterElement.innerHTML = `<img src="${Poster}"/>`
+
+  movieContainer.appendChild(moviePosterElement);
+  movieContainer.appendChild(movieElement);
+
 
 };
 
@@ -59,4 +85,11 @@ searchFrom.addEventListener('submit', (e) => {
   if (movieName !== '') {
     getMovieInfo(movieName);
   }
+  else{
+    movieContainer.innerHTML=`<h2> Enter Movie Name To Get Movie Information </h2>`
+    movieContainer.classList.remove('movie-container')
+    movieContainer.classList.add('noBackground')
+
+  }
+movieContainer.classList.add('movie-container')
 })
